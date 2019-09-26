c1de216b688df397dafdc5cb0ac042d0f2ef8e17
nickhudson4 <nickkhudson2244@gmail.com>
2019-09-26 06:34:13 -0400

    Initial commit

---
+++ Assets/Scipts/Maze/MazeGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGenerator : MonoBehaviour
+{
+    public GameObject floorQuad;
+    void Start(){
+        setupMatrix(floorQuad);
+    }
+
+    void Update(){
+        
+    }
+
+    private void setupMatrix(GameObject floor){
+        float floor_scaleX = floor.transform.localScale.x;
+        float floor_scaleY= floor.transform.localScale.y;
+
+        float spread = 0.05f;
+        int num_rows = (int)(floor_scaleY / spread);
+        int num_cols = (int)(floor_scaleX / spread);
+
+        int[,] matrix = new int[num_rows, num_cols];
+
+        
+
+
+
+    }
+}
---
+++ Assets/Scipts/Player/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+
+    //Movement Vars
+    public float speed = 1;
+    void Start(){
+       rb = GetComponent<Rigidbody>(); 
+    }
+
+    void Update(){
+        handleMovement();
+    }
+
+    private void handleMovement(){
+         if (Input.GetKey(KeyCode.A))
+            moveOneStep(-Vector3.right);
+         if (Input.GetKey(KeyCode.D))
+            moveOneStep(Vector3.right);
+         if (Input.GetKey(KeyCode.W))
+            moveOneStep(Vector3.up);
+         if (Input.GetKey(KeyCode.S))
+            moveOneStep(-Vector3.up);
+    }
+
+    private void moveOneStep(Vector3 dir){
+        rb.MovePosition(transform.position + (dir * Time.deltaTime));
+    } 
+
+    public Vector2 vector2_to_vector3(Vector3 orig, string dropAxis){
+        if (dropAxis == "y"){
+            return new Vector2(orig.x, orig.z);
+        }
+        if (dropAxis== "x"){
+            return new Vector2(orig.y, orig.z);
+        }
+        else {
+            return new Vector2(orig.x, orig.y);
+        }
+    }
+}
